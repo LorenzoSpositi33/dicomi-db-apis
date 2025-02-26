8dd56de2aaf5601fe76c4befc68d85aeda093e38
Luca <luca.dozio@iceinformatics.com>
2025-02-26 14:11:33 +0100

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,12 @@ module.exports = {
       autorestart: true,
       max_memory_restart: "1G",
       restart_delay: 5000,
-      min_uptime: 5000
+      min_uptime: 5000,
+      error_file: "./logs/errors.log",
+      out_file: "./logs/app.log",
+      log_file: "./logs/full.log",
+      time: true,
+      merge_logs: true,
     }
   ]
 };
