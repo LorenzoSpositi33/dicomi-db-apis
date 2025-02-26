33b49e6e4d5904135a60114e2454c758df966b3b
Luca <luca.dozio@iceinformatics.com>
2025-02-26 13:40:56 +0100

    New

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "node-dicomi-db-apis",
+      script: "./.dist/main.js",
+      autorestart: true,
+      max_memory_restart: "1G",
+      restart_delay: 5000,
+      min_uptime: 5000
+    }
+  ]
+};
