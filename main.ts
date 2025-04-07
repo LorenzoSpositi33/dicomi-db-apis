3069af278f69329a4f170e4b75627961bd628cc3
Luca <luca.dozio@iceinformatics.com>
2025-04-07 17:49:46 +0200

    Aggiunta gestione files Dicomi, Consegnato
    Ora c'è un servizio in ascolto perenne su una cartella dove vengono storati i file di Dicomi provenienti da Q8. La funzione del consegnato legge il csv, lo interpreta, e fa un upsert in DB

--- main.ts
+++ main.ts
@@ -6,24 +6,36 @@ import crypto from "crypto";
 // Aggiunto per HTTPS
 import https from "https";
 import fs from "fs";
+import path from "path";
+import csv from "csv-parser";
 
 dotenv.config();
 
 // Parametri del file .ENV
 const apiPort = process.env.DB_API_PORT;
 const concTable = process.env.TABLE_CONCORRENZA;
+const impiantiTable = process.env.TABLE_IMPIANTI;
+const consegnatoTable = process.env.TABLE_CONSEGNATO;
+const artSellInTable = process.env.TABLE_ARTICOLI_SELLIN;
+const artTable = process.env.TABLE_ARTICOLI;
 const secretGen = process.env.SECRET_AUTH_KEY;
+const csvMainDirectory = String(process.env.FILE_MAIN_FOLDER);
+const csvInputDirectory = String(process.env.FILE_FOLDER_INPUT);
+const csvOkDirectory = String(process.env.FILE_FOLDER_OK);
+const csvErrDirectory = String(process.env.FILE_FOLDER_ERR);
 
 const app = express();
 
 // Leggi i file dei certificati SSL
 const privateKey = fs.readFileSync("C:/Certificati/cert_dicosv001.key", "utf8");
-const certificate = fs.readFileSync("C:/Certificati/cert_dicosv001.crt", "utf8");
+const certificate = fs.readFileSync(
+  "C:/Certificati/cert_dicosv001.crt",
+  "utf8"
+);
 const credentials = { key: privateKey, cert: certificate };
 
 // Crea il server HTTPS usando l'app Express
 const httpsServer = https.createServer(credentials, app);
-
 
 // Funzione per calcolare l'hash di una stringa
 function hashString(input: string): string {
@@ -64,6 +76,342 @@ async function replaceOss(oldId: string, newId: string): Promise<boolean> {
   return false;
 }
 
+async function impiantiMapping(): Promise<String[]> {
+  let result: String[] = [];
+
+  try {
+    const pool = await getDatabasePool();
+
+    const query = `SELECT imp.ImpiantoCodice AS imp
+    
+    FROM ${impiantiTable} AS imp`;
+
+    result = (await pool.request().query(query)).recordset.map(
+      (item) => item.imp
+    );
+  } catch (err) {
+    console.error(
+      `Errore nell'ottenere l'elenco degli articoli sell-in, query`,
+      err
+    );
+  }
+
+  return result;
+}
+
+async function sellInMapping(): Promise<Record<string, string>> {
+  let result: Record<string, string> = {};
+
+  try {
+    const pool = await getDatabasePool();
+
+    const query = `SELECT ars.Articolo AS art, ars.ArticoloSellin AS artSellin
+    
+    FROM ${artSellInTable} AS ars
+
+    INNER JOIN ${artTable} AS arb
+    ON arb.Articolo=ars.Articolo`;
+
+    const recordset = (await pool.request().query(query)).recordset;
+
+    const mapping = recordset.reduce((acc, item) => {
+      acc[item.artSellin] = item.art;
+      return acc;
+    }, {} as Record<string, string>);
+
+    result = mapping;
+  } catch (err) {
+    console.error(
+      `Errore nell'ottenere l'elenco degli articoli sell-in, query`,
+      err
+    );
+  }
+
+  return result;
+}
+
+// Funzione per fare upsert riga per riga per il Consegnato
+async function upsertConsegnato(
+  impiantoCodice: string,
+  ArticoloSellin: string,
+  dataConsegna: Date,
+  articolo: string,
+  consegnato: number
+) {
+  console.log(impiantoCodice, ArticoloSellin, dataConsegna, consegnato);
+
+  //.toISOString().split("T")[0];
+  try {
+    const pool = await getDatabasePool();
+    // La query MERGE controlla se esiste già una riga con le stesse chiavi (impiantoCodice, Articolo, DataConsegna):
+    // se sì, esegue l'UPDATE, altrimenti esegue l'INSERT.
+    const query = `
+        MERGE INTO ${consegnatoTable} AS target
+        USING (VALUES (@ImpiantoCodice, @ArticoloSellin, @DataConsegna))
+          AS source (ImpiantoCodice, ArticoloSellin, DataConsegna)
+        ON (
+          target.ImpiantoCodice = source.ImpiantoCodice
+          AND target.ArticoloSellin = source.ArticoloSellin
+          AND target.DataConsegna = source.DataConsegna
+        )
+        WHEN MATCHED THEN
+          UPDATE SET
+          Articolo = @Articolo,
+          QtaConsegnata = @QtaConsegnata,
+          DataModifica = GETDATE()
+        WHEN NOT MATCHED THEN
+            INSERT (DataConsegna, ImpiantoCodice, Articolo, ArticoloSellin, QtaConsegnata)
+            VALUES (@DataConsegna, @ImpiantoCodice, @Articolo, @ArticoloSellin, @QtaConsegnata);
+      `;
+
+    const result = await pool
+      .request()
+      .input("ImpiantoCodice", sql.NVarChar, impiantoCodice)
+      .input("ArticoloSellin", sql.NVarChar, ArticoloSellin)
+      .input("DataConsegna", sql.Date, dataConsegna)
+      .input("Articolo", sql.NVarChar, articolo)
+      .input("QtaConsegnata", sql.Float, consegnato)
+      .query(query);
+
+    if (result.rowsAffected[0] > 0) {
+      logger.info(
+        `Upsert eseguito con successo: ${consegnatoTable}: ${impiantoCodice}, ${ArticoloSellin}, ${articolo}, ${dataConsegna}, ${consegnato}`
+      );
+      return true;
+    } else {
+      logger.warn(
+        `Nessuna riga è stata aggiornata/inserita: ${consegnatoTable}: ${impiantoCodice}, ${ArticoloSellin}, ${articolo}, ${dataConsegna}, ${consegnato}`
+      );
+      return false;
+    }
+  } catch (err) {
+    logger.error(
+      `Errore durante l'upsert della riga di Consegnato: ${impiantoCodice}, ${ArticoloSellin}, ${articolo}, ${dataConsegna}, ${consegnato}`,
+      err
+    );
+    return false;
+  }
+}
+
+async function elaboraConsegnato(results: any[], fileHeaders: String[]) {
+  const sellinMap = await sellInMapping();
+  const impiantiMap = await impiantiMapping();
+
+  const expectedHeaders = ["PV", "PRODOTTO", "DATA_CONSEGNA", "CONSEGNATO"];
+
+  // 1) Controllo che gli header del file siano corretti
+  if (!(await equalList(fileHeaders, expectedHeaders))) {
+    console.error(
+      `Errore: gli header del file non sono quelli previsti. Attuali: ${fileHeaders} vs Previsti ${expectedHeaders}`
+    );
+    return false;
+  }
+
+  // Elaboro il contenuto, riga per riga, del file e interagisco con il DB
+  for (const row of results) {
+    // Ottengo il prodotto (ArticoloSellIn)
+    const prodotto = String(row.PRODOTTO);
+
+    // Ottengo la data consegna formattata correttamente
+    const oldDataConsegna = String(row.DATA_CONSEGNA);
+    const [dayDataConsegna, monthDataConsegna, yearDataConsegna] =
+      oldDataConsegna.split("/");
+    const dataConsegna = new Date(
+      Number(yearDataConsegna),
+      Number(monthDataConsegna) - 1,
+      Number(dayDataConsegna)
+    );
+
+    // Ottengo l'impianto codice con 4 cifre forzate, e gli 0 davanti in caso servano per completare la stringa
+    const impiantoCodice = String(row.PV).padStart(4, "0");
+
+    // Ottengo la quantità di consegnato nel formato corretto, pronto per l'inserimento nel DB
+    const consegnato =
+      parseFloat(String(row.CONSEGNATO).replace(",", ".")) / 1000;
+
+    // CONTROLLO RIGA PER RIGA SE E' VALIDA, ALTRIMENTI LA IGNORO
+
+    // 1) Controllo se la quantità consegnata è minore di 0
+    if (consegnato < 0 || !consegnato) {
+      console.error(
+        "Il valore di consegnato contiene una quantità negativa o nulla: ",
+        row
+      );
+      continue;
+    }
+
+    const art = sellinMap[prodotto];
+
+    // 2) Controllo se il prodotto è valido, in base all'anagrafica nel DB
+    if (!art) {
+      console.error(
+        "Il valore di prodotto non è presente nel database di Dicomi: ",
+        row
+      );
+      continue;
+    }
+
+    // 3) Controllo se l'impianto è valido, in base all'anagrafica nel DB
+    if (!impiantiMap.includes(impiantoCodice)) {
+      console.error(
+        "Il valore di impianto non è presente nel database di Dicomi: ",
+        row
+      );
+      continue;
+    }
+
+    // Faccio l'upsert sul DB
+    await upsertConsegnato(
+      impiantoCodice,
+      prodotto,
+      dataConsegna,
+      art,
+      consegnato
+    );
+  }
+
+  return true;
+}
+
+// Confronto contenuto array, ritorna true se sono uguali a livello di contenuti, oppure false
+async function equalList(list1: String[], list2: String[]): Promise<boolean> {
+  return (
+    list1.length === list2.length &&
+    list2.every((expected) =>
+      list1.some((header) => header.toLowerCase() === expected.toLowerCase())
+    )
+  );
+}
+
+async function moveFile(status: boolean, filePath: string) {
+  // Se status è vero, usa la cartella OK, altrimenti quella ERR
+
+  const targetDirectory = status
+    ? csvMainDirectory + `/${csvOkDirectory}`
+    : csvMainDirectory + `/${csvErrDirectory}`;
+
+  // Estrai il nome originale del file e la sua estensione
+  const originalFileName = path.basename(filePath);
+  const ext = path.extname(originalFileName);
+  const baseName = path.basename(originalFileName, ext);
+
+  // Crea un timestamp formattato come YYYYMMDD_HHMMSS
+  const timestamp = new Date()
+    .toISOString() // "2025-04-07T13:14:15.678Z"
+    .replace(/[-:]/g, "") // "20250407T131415.678Z"
+    .slice(0, 15) // "20250407T131415"
+    .replace("T", "_"); // "20250407_131415"
+
+  // Costruisci il nuovo nome del file
+  const newFileName = `${baseName}_${timestamp}${ext}`;
+  const targetPath = path.join(targetDirectory, newFileName);
+
+  try {
+    await fs.promises.rename(filePath, targetPath);
+    console.log(`File spostato in: ${targetPath}`);
+  } catch (err) {
+    console.error("Errore nello spostamento del file:", err);
+  }
+}
+
+// CONTROLLO FILES continuo, per import csv di Q8
+async function controlloFiles() {
+  const inputDirectory = csvMainDirectory + `/${csvInputDirectory}`;
+
+  try {
+    // Ottengo tutti i files nella cartella di input
+    const files = await fs.promises.readdir(inputDirectory);
+
+    // Se è presente almeno un file
+    if (files.length > 0) {
+      console.log("File rilevati:", files);
+
+      for (const file of files) {
+        const filePath = path.join(inputDirectory, file);
+        console.log("File in elaborazione:", file);
+
+        // Il file è del formato corretto CSV
+        if (path.extname(file).toLowerCase() === ".csv") {
+          // Leggo il contenuto del file
+          const results: any[] = [];
+          // Headers del file
+          let fileHeaders: String[] = [];
+
+          // Incapsula la lettura in una Promise
+          await new Promise((resolve, reject) => {
+            fs.createReadStream(filePath)
+              .pipe(csv({ separator: ";" }))
+              .on("headers", (headers) => {
+                fileHeaders = headers;
+              })
+              .on("data", (row) => {
+                // Accumula ogni riga
+                results.push(row);
+              })
+              .on("end", () => {
+                console.log("Il file CSV è stato letto correttamente");
+                resolve(results);
+              })
+              .on("error", (err) => {
+                console.error("Errore nella lettura del file CSV:", err);
+                reject(err);
+              });
+          });
+
+          // CONTROLLO SE IL FILE NON SIA VUOTO
+          if (results.length === 0) {
+            console.error("Errore: il file non contiene righe di dati.");
+            await moveFile(false, filePath);
+            continue;
+          }
+
+          // Capisco la categoria del file
+          switch (true) {
+            // Il file è "CONSEGNATO"
+            case /consegnato/i.test(file):
+              console.log("File riconosciuto come CONSEGNATO: ", file);
+
+              await moveFile(
+                await elaboraConsegnato(results, fileHeaders),
+                filePath
+              );
+
+              break;
+            // Il file non è stato riconosciuto
+            default:
+              console.log("File non riconosciuto: ", file);
+          }
+        }
+        // Il file non è in formato CSV
+        else {
+          await moveFile(false, filePath);
+          console.log("Il file contiene un'estensione errata: ", file);
+        }
+      }
+
+      //TODO: Da rimuovere:
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+    // Se non è presente neanche un file
+    else {
+      console.log("Nessun file .csv trovato nella cartella", inputDirectory);
+
+      // Sleep di 1 minuto
+      await new Promise((resolve) => setTimeout(resolve, 60000));
+    }
+  } catch (error) {
+    console.error(
+      "Errore durante la lettura della cartella",
+      csvMainDirectory,
+      error
+    );
+  }
+
+  // Ora richiama la funzione per eseguire il prossimo ciclo
+  controlloFiles();
+}
+
+// ENDPOINT per sostituire l'osservatorio ID
 app.get(
   "/updateConcorrenteOss",
   async (req: Request, res: Response): Promise<any> => {
@@ -308,6 +656,9 @@ app.get(
     }
   }
 );
+
+// Inizio del pooling
+controlloFiles();
 
 httpsServer.listen(apiPort, () => {
   logger.info(`Servizio avviato e in ascolto sulla porta HTTPS: ${apiPort}`);
